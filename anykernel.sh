4e9850d8cfbc9bbb65d4598f76330963f10a31e7
sunilpaulmathew <sunil.kde@gmail.com>
2018-11-09 12:18:20 +0900

    Apply SmartPack specific changes The changes are specific for my op5t Oreo kernel. Other devices requires proper changes mainly in the anykernel script.
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,23 +1,28 @@
-# AnyKernel2 Ramdisk Mod Script
-# osm0sis @ xda-developers
+#
+# SmartPack-Kernel (AnyKernel) Script
+#
+# Credits: osm0sis @ xda-developers
+#
+# Modified by sunilpaulmathew@xda-developers.com
+#
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=SmartPack Kernel by sunilpaulmathew@xda-developers.com
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
+device.name1=OnePlus5T
+device.name2=dumpling
+device.name3=OnePlus5
+device.name4=cheeseburger
 device.name5=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -35,29 +40,70 @@ chown -R root:root $ramdisk/*;
 
 
 ## AnyKernel install
+
+# Don't allow flashing on non-Treble ROMs
+is_treble=$(file_getprop /system/build.prop "ro.treble.enabled");
+if [ ! "$is_treble" -o "$is_treble" == "false" ]; then
+  ui_print " ";
+  ui_print "This version of SmartPack-Kernel is only compatible with stock OxygenOS 5.1.5, or newer with Treble support!";
+  exit 1;
+fi;
+
+# Don't allow flashing on unsupported versions
+ui_print "Checking android version...";
+android_ver=$(file_getprop /system/build.prop "ro.build.version.release");
+ui_print "Android $android_ver detected...";
+ui_print " ";
+if [ ! "$android_ver" == "8.1.0" ]; then
+  ui_print "This version of SmartPack-Kernel is only compatible with Android 8.1.0!";
+  exit 1;
+fi;
+
+# Apply patched wifi config only for OP5
+userflavor="$(file_getprop /system/build.prop "ro.build.user"):$(file_getprop /system/build.prop "ro.build.flavor")";
+if [ ! "$userflavor" == "OnePlus:OnePlus5-user" ]; then
+  rm $ramdisk/WCNSS_qcom_cfg.ini
+fi;
+
 dump_boot;
 
 # begin ramdisk changes
 
 # init.rc
 backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+grep "import /init.SmartPack.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.SmartPack.rc\n&/' init.rc
+grep "import /init.spectrum.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.spectrum.rc\n&/' init.rc
 
 # init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
 
 # fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+
+# Misc tweaks (Credits: Render Zenith Kernel)
+# sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+    "allow init rootfs file execute_no_trans" \
+    "allow { init modprobe } rootfs system module_load" \
+    "allow init { system_file vendor_file vendor_configs_file } file mounton" \
+    ;
+
+# sepolicy_debug
+$bin/magiskpolicy --load sepolicy_debug --save sepolicy_debug \
+    "allow init rootfs file execute_no_trans" \
+    "allow { init modprobe } rootfs system module_load" \
+    "allow init { system_file vendor_file vendor_configs_file } file mounton" \
+    ;
+
+# Remove recovery service so that TWRP isn't overwritten
+remove_section init.rc "service flash_recovery" ""
+
+# Kill init's search for Treble split sepolicy if Magisk is not present
+# This will force init to load the monolithic sepolicy at /
+if [ ! -d .backup ]; then
+    sed -i 's;selinux/plat_sepolicy.cil;selinux/plat_sepolicy.xxx;g' init;
+fi;
 
 # end ramdisk changes
 
 write_boot;
 
 ## end install
-
